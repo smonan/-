65048d65cd894c1516283d845f057337ea9a97f5
smonan <128983172+smonan@users.noreply.github.com>
2023-03-26 21:12:53 +0700

    Add files via upload

---
+++ questions-1.js
@@ -0,0 +1,112 @@
+let start_btn = [
+    {
+    numb: 1,
+    question: "HTML ย่อมาจากอะไร?",
+    answer: "HyperText Markup Language",
+    options: [
+      "HyperText Markup Language",
+      "HyperTest Makeup Language",
+      "HyperText Markup Landscape",
+      "HyperTest Makeup Landscape"
+    ]
+  },
+    {
+    numb: 2,
+    question: "การเขียนหรือการใช้รูปแบบคำสั่งควรใส่ไว้ในระหว่างเครื่องหมายใด?",
+    answer: "แท็ก(Tag)",
+    options: [
+      "เซมิโคลอน (Semicolon)",
+      "แท็ก(Tag)",
+      "ซินเท็ก(Syntax)",
+      "อัญประกาศ(Quotation Marks)"
+    ]
+  },
+    {
+    numb: 3,
+    question: "รูปแบบคำสั่งที่อยู่ในแท็กแบ่งได้เป็นกี่ลักษณะ?",
+    answer: "2 ลักษณะ แท็กเดี่ยว และแท็กปิด",
+    options: [
+      "มีลักษณะเดียวคือ แท็กเดี่ยว",
+      "2 ลักษณะ แท็กเดี่ยว และแท็กปิด",
+      "3 ลักษณะ แท็กเดี่ยว แท็กปิด และไม่ใส่แท็ก",
+      "ไม่มีข้อถูก"
+    ]
+  },
+    {
+    numb: 4,
+    question: "นามสกุลของ HTML Document คือนามสกุลอะไร?",
+    answer: ".html",
+    options: [
+      ".html",
+      ".txt",
+      ".exe",
+      "นามสกุลไฟล์คืออยู่ที่เรากำหนด"
+    ]
+  },
+    {
+    numb: 5,
+    question: "แท็ก &lt;titl&gt;...&lt;/title&gt; ใช้ทำอะไร?",
+    answer: "ใช้ในการบอกชื่อเรื่องของหน้าเว็บนั้นๆ",
+    options: [
+      "เป็นแท็กใช้กำหนดปัญหา",
+      "ใช้กำหนดหัวข้อ",
+      "ใช้ในการบอกชื่อเรื่องของหน้าเว็บนั้นๆ",
+      "ไม่มีข้อถูก"
+    ]
+  },
+  {
+    numb: 6, 
+    question: "HTML สามารถใช้โปรแกรมใดต่อไปนี้เขียนได้",
+    answer: "ทุกข้อที่กล่าว",
+    options: [
+      "Notepad",
+      "Editplus",
+      "Visual Studio Code",
+      "ทุกข้อที่กล่าว"
+    ]
+  },
+    {
+    numb: 7,
+    question: "คุณลักษณะ(Attributes) มีหน้าที่อะไร?",
+    answer: "เป็นส่วนขยายความสามารถของแท็ก",
+    options: [
+      "เป็นส่วนขยายความสามารถของแท็ก",
+      "เป็นการมาร์กข้อความไว้",
+      "เป็นการคอมเมนต์ข้อความไว้",
+      "เป็นการจัดระเบียบแท็ก"
+    ]
+  },
+    {
+    numb: 8,
+    question: "คำสั่งใดเป็นคำสั่ง คุณลักษณะ(Attributes)",
+    answer: "WIDTH",
+    options: [
+      "IMG",
+      "FORM",
+      "WIDTH",
+      "TABLE"
+    ]
+  },
+  {
+    numb: 9,
+    question: "คำสั่งใดเป็นคำสั่งเป็นคำสั่ง 'แท็กแบบเดี่ยว'",
+    answer: "HR",
+    options: [
+      "HR",
+      "FONT",
+      "P",
+      "B"
+    ]
+  },
+    {
+    numb: 10,
+    question: "ข้อใด'ไม่ใช่'คำสั่งองค์ประกอบ(Elements)ของภาษา HTML",
+    answer: "FOOTER",
+    options: [
+      "HTML",
+      "HEAD",
+      "BODY",
+      "FOOTER"
+    ]
+  },
+];
